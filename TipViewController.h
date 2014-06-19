63dcc74b1c13b4baea427d0533992041f473c062
tko <tko@tko-macbookpro.roam.corp.google.com>
2014-06-18 17:09:48 -0700

    iOS for Designers Homework #0

---
+++ TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Taeho Ko on 6/17/14.
+//  Copyright (c) 2014 google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
